df8ca375e204e490cb3e40854673c053bc870667
Rafael <Alonso>
2019-09-25 23:40:51 -0300

    Almost done

--- des.c
+++ des.c
@@ -7,15 +7,16 @@
   char bitAt(char *string, int position);
 
   void stringToBits(char *string);
-  long stringToLong(char *string);
   char* longToString(unsigned long textInLong);
 
-  char* initial_permutation(char *plain_text);
-  void permutation(char **to_permute, int pm);
-  char* final_permutation(char *plain_text);
-  void xor(char **first, char **second);
-  void sbox(char **part, short round);
-  void join_and_swap(char **left, char **right);
+  unsigned long initial_permutation(char *plain_text);
+  unsigned long permutation(char *to_permute, const int pm[], int size);
+  unsigned long final_permutation(char *plain_text);
+
+  unsigned long circularLeftShift(unsigned long part, int nShifts);
+
+  char sbox(char part, short round);
+  unsigned long joinSplits(char *splits);
 
   // Initial Permutation Matrix
   const int IP[] = {
@@ -66,61 +67,61 @@
   };
 
   // ================================ SBOXES ================================
-  const int S1[4][16]  = {
-     14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7,
-      0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8,
-      4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0,
-     15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13
-  };
+    const int S1[4][16]  = {
+       14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7,
+        0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8,
+        4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0,
+       15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13
+    };
 
-  const int S2[4][16]  = {
-     15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10,
-      3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5,
-      0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15,
-     13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9
-  };
+    const int S2[4][16]  = {
+       15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10,
+        3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5,
+        0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15,
+       13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9
+    };
 
-  const int S3[4][16]  = {
-     10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8,
-     13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1,
-     13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7,
-      1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12
-  };
+    const int S3[4][16]  = {
+       10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8,
+       13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1,
+       13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7,
+        1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12
+    };
 
-  const int S4[4][16]  = {
-      7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11, 12,  4, 15,
-     13,  8, 11,  5,  6, 15,  0,  3,  4,  7,  2, 12,  1, 10, 14,  9,
-     10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4,
-      3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14
-  };
+    const int S4[4][16]  = {
+        7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11, 12,  4, 15,
+       13,  8, 11,  5,  6, 15,  0,  3,  4,  7,  2, 12,  1, 10, 14,  9,
+       10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4,
+        3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14
+    };
 
-  const int S5[4][16]  = {
-      2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9,
-     14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6,
-      4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14,
-     11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3
-  };
+    const int S5[4][16]  = {
+        2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9,
+       14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6,
+        4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14,
+       11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3
+    };
 
-  const int S6[4][16]  = {
-     12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11,
-     10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8,
-      9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6,
-      4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13
-  };
+    const int S6[4][16]  = {
+       12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11,
+       10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8,
+        9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6,
+        4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13
+    };
 
-  const int S7[4][16] = {
-      4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1,
-     13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6,
-      1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2,
-      6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12
-  };
+    const int S7[4][16] = {
+        4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1,
+       13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6,
+        1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2,
+        6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12
+    };
 
-  const int S8[4][16] = {
-     13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7,
-      1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2,
-      7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8,
-      2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11
-  };
+    const int S8[4][16] = {
+       13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7,
+        1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2,
+        7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8,
+        2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11
+    };
   // ========================================================================
 
   // Permutation Choice 1 Matrix
@@ -151,56 +152,144 @@
 // </editor-fold>
 
 int main( int argc, char *argv[] ){
+  unsigned long entradaManipulada;
+  unsigned long entradaEsq;
+  unsigned long entradaDir;
+  char eightSplit[8] = "";
+
+  unsigned long chaveManipulada;
+  unsigned long chaveEsq;
+  unsigned long chaveDir;
+
+  char entrada[9];
+  char chave[9];
+
   // get the 64-bit plain text
+  strcpy(entrada, argv[0]);
+  entrada[8] = '\0';
+
   // pass it through the initial permutation (IP)   => 64-bit
+  entradaManipulada = initial_permutation(entrada);
 
   // get the 64-bit key
+  strcpy(chave, argv[1]);
+  chave[8] = '\0';
   // pass it through the permutation choice 1 (PC1) => 56-bit
+  chaveManipulada = permutation(chave, PC1, 56);
+
+  stringToBits(chave);
+  stringToBits(longToString(chaveManipulada));
+  printf("\n");
 
   // =============================== ROUNDS ===============================
   // for each of the 16 rounds:
+  for(char i = 0; i < 15; i++){
     // ============================= KEY PART =============================
     // divide the 56-key in two => 28-bit / 28-bit
+    chaveEsq = chaveManipulada >> 28;
+    chaveDir = chaveManipulada << 36 >> 36;
+
+    stringToBits(longToString(chaveEsq));
+    stringToBits(longToString(chaveDir));
+    printf("\n");
+
     // shift the two parts to the left and join them  => 56-bit
+    chaveEsq = circularLeftShift(chaveEsq, SHIFTS[i]);
+    chaveDir = circularLeftShift(chaveDir, SHIFTS[i]);
+
+    stringToBits(longToString(chaveEsq));
+    stringToBits(longToString(chaveDir));
+    printf("\n");
+
+    chaveManipulada = (chaveEsq << 28) | chaveDir;
+    stringToBits(longToString(chaveManipulada));
+    printf("\n");
+
     // permutate the shifted key with permutation choice 2 (PC2) => 48-bit
+    chaveManipulada = permutation(longToString(chaveManipulada), PC2, 48);
+    stringToBits(longToString(chaveManipulada));
+    printf("\n");
+
     // ====================================================================
 
     // ============================= TXT PART =============================
     // divide the text in two => 32-bit / 32-bit
+    entradaEsq = entradaManipulada >> 32;
+    entradaDir = entradaManipulada << 32 >> 32;
+
+    stringToBits(longToString(entradaEsq));
+    stringToBits(longToString(entradaDir));
+    printf("\n");
 
     // ============= RIGHT PART =============
     // pass it through the expansion (E) => 48-bit
+    entradaDir = permutation(longToString(entradaDir), E, 48);
+    stringToBits(longToString(entradaDir));
+    printf("\n");
+
     // XOR the right side with the PC2'd key => 48-bit
+    entradaDir = entradaDir ^ chaveManipulada;
+    stringToBits(longToString(entradaDir));
+    printf("\n");
+
     // with the result:
+    for(char j = 0; j < 8; j++){
       // divide it into eight => 6-bit / 6-bit / 6-bit ...
+      eightSplit[j] = (char) (entradaDir << (16 + 6 * j) >> 58);
+      // stringToBits(eightSplit);
+
       // pass each part through their respective Sbox (S1, S2, ...)
-      // join them => 32-bit
+      eightSplit[j] = sbox(eightSplit[j], j + 1);
+      // stringToBits(eightSplit);
+
+    }
+    stringToBits(eightSplit);
+    // join them => 32-bit
+    entradaDir = joinSplits(eightSplit);
+    stringToBits(longToString(entradaDir));
+    printf("\n");
+
     // pass it through the permutation (P) => 32-bit
+    entradaDir = permutation(longToString(entradaDir), P, 32);
+    stringToBits(longToString(entradaDir));
+    printf("\n");
+
     // XOR it with the left side
+    entradaDir = entradaDir ^ entradaEsq;
+    stringToBits(longToString(entradaDir));
+    printf("\n");
+
     // ======================================
 
     // join the left and right sides swaped => 64-bit
+    entradaManipulada = (entradaDir << 32) | entradaEsq;
+    stringToBits(longToString(entradaManipulada));
+    printf("\n");
+    break;
     // ====================================================================
+  }
   // ======================================================================
 
   // divide the result into two => 32-bit / 32-bit
+  entradaEsq = entradaManipulada >> 32;
+  entradaDir = entradaManipulada << 32 >> 32;
+
+  stringToBits(longToString(entradaEsq));
+  stringToBits(longToString(entradaDir));
+  printf("\n");
+
   // join them swaped
+  entradaManipulada = (entradaDir << 32) | entradaEsq;
+  stringToBits(longToString(entradaManipulada));
+  printf("\n");
+
   // pass it through with the final permutation (FP)
+  entradaManipulada = final_permutation(longToString(entradaManipulada));
 
-  char entrada[8] = "hi world";
-  char* res;
-  int auxA;
-  int auxB;
-  int pos;
+  FILE *f = fopen("out.txt", "w");
+  fprintf(f, "%s\n", longToString(entradaManipulada));
 
-  stringToBits(entrada);
-
-  res = initial_permutation(entrada);
-  stringToBits(res);
-  res = final_permutation(res);
-  stringToBits(res);
-
-  free(res);
+  fclose(f);
   return 0;
 }
 
@@ -224,15 +313,6 @@ void stringToBits(char *string){
   printf("\n");
 }
 
-long stringToLong(char *string){
-  unsigned long res = 0;
-
-  for(int i = 7; i >= 0; i--){
-    res += (unsigned long) string[i] << (8 * (7 - i));
-  }
-  return res;
-}
-
 char* longToString(unsigned long textInLong){
   char* res = (char *) malloc(8 * sizeof(char));
   for(int i = 7; i >= 0; i--){
@@ -241,7 +321,7 @@ char* longToString(unsigned long textInLong){
   return res;
 }
 
-char* initial_permutation(char *plain_text){
+unsigned long initial_permutation(char *plain_text){
   // given the plain_text, get it's bits
   // rearrange them according to the position pointed out in IP
   unsigned long res = 0;
@@ -251,10 +331,10 @@ char* initial_permutation(char *plain_text){
     }
   }
 
-  return longToString(res);
+  return res;
 }
 
-char* final_permutation(char *plain_text){
+unsigned long final_permutation(char *plain_text){
   // given the plain_text, get it's bits
   // rearrange them according to the position pointed out in IP
   unsigned long res = 0;
@@ -264,29 +344,71 @@ char* final_permutation(char *plain_text){
     }
   }
 
-  return longToString(res);
+  return res;
 }
 
-void permutation(char **to_permute, int pm){
+unsigned long permutation(char *to_permute, const int pm[], int size){
   // change the place of each bit of given part, placing it in the
   // position specified by the given permutation matrix
+  unsigned long res = 0;
+  for(int i = 0; i < size; i++){
+    if (bitAt(to_permute, pm[i])){
+      res += (unsigned long) pow(2, size - 1 - i);
+    }
+  }
+
+  return res;
 }
 
-void xor(char **first, char **second){
-  // given two sets of bits, store in the result their XOR
-  // (or exclusive)
+unsigned long circularLeftShift(unsigned long part, int nShifts){
+  part = part << nShifts;
+
+  if (bitAt(longToString(part), 36)) part -= pow(2, 28);
+
+  if (nShifts == 1){
+    return part + bitAt(longToString(part), 36);
+  } else{
+    if (bitAt(longToString(part), 35)) part -= pow(2, 29);
+    return part + bitAt(longToString(part), 36) + (2 * bitAt(longToString(part), 35));
+  }
 }
 
-void sbox(char **part, short part_round){
+char sbox(char part, short part_round){
   // given the 6-bit part and which one it is (1 - 8),
   // get the first and last bit of it => 2-bit
+  char external = part & (1 << 6) * 2 + part & 1;
   // get all the others separately    => 4-bit
+  char internal = part & (15 << 1);
   // the 2-bit part indicate the SBox row
   // the 4-bit part indicate the SBox column
   // return the value indicated in the found position
+
+  char res;
+  switch(part_round){
+    case 1:
+      res = S1[external][internal];
+    case 2:
+      res = S2[external][internal];
+    case 3:
+      res = S3[external][internal];
+    case 4:
+      res = S4[external][internal];
+    case 5:
+      res = S5[external][internal];
+    case 6:
+      res = S6[external][internal];
+    case 7:
+      res = S7[external][internal];
+    case 8:
+      res = S8[external][internal];
+  }
+  return res;
 }
 
-void join_and_swap(char **left, char **right){
-  // given two parts (left and right), return them joined
-  // right part first, left part second
+unsigned long joinSplits(char *splits){
+  unsigned long res = 0;
+  for(int i = 7; i >= 0; i--){
+    res += (unsigned long) ( (unsigned long) splits[i] << (4 * (7 - i)));
+  }
+  return res;
 }
